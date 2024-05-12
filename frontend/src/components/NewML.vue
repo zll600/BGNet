56e24465243267fc9840058a26b14b173a11034b
zll600 <3400692417@qq.com>
2024-05-12 16:06:55 +0800

    feat: use two upload ui

--- frontend/src/components/NewML.vue
+++ frontend/src/components/NewML.vue
@@ -11,28 +11,39 @@
       </div>
     </div>
     <div id="content" class="flex flex-row flex-wrap justify-between content-stretch h-5/6 p-2">
-      <div id="left" class="flex flex-col justify-between items-center content-between w-2/5">
+      <div id="left" class="flex flex-col justify-evenly items-center content-between w-2/5">
         <label class="font-mono text-lg">上传两张照片，原图是 jpg 格式，GT 是 png 格式</label>
-        <div id="upload-image" class="w-full h-1/2 flex flex-col items-center content-center">
+        <div
+          id="upload-image"
+          class="w-full h-1/2 flex flex-col justify-center items-center content-center"
+        >
           <NUpload
-            class="w-full bg-sky-100"
             accept="image/*"
             list-type="image-card"
+            :max="1"
             action="http://127.0.0.1:8080/uploadv2"
-            v-model:file-list="fileList"
-            @change="handleUploadChange"
+            v-model:file-list="fileListImg"
+            @change="handleUploadImgChange"
             @remove="handleRemove"
             @update:file-list="handleFileListChange"
           />
-          <NImage
-            v-if="previewImageUrlRef1.length > 0"
-            :src="previewImageUrlRef1"
-            :alt="`Uploaded Image 1`"
+          <NDivider></NDivider>
+          <NUpload
+            accept="image/*"
+            list-type="image-card"
+            :max="1"
+            action="http://127.0.0.1:8080/uploadv2"
+            v-model:file-list="fileListGT"
+            @change="handleUploadGTChange"
+            @remove="handleRemove"
+            @update:file-list="handleFileListChange"
           />
         </div>
       </div>
       <div id="button" class="w-1/5 flex justify-center items-center content-center">
-        <NButton type="primary" :loading="loading" @click="confirmUpload">生成结果</NButton>
+        <NButton type="primary" round size="large" :loading="loading" @click="confirmUpload"
+          >生成结果</NButton
+        >
       </div>
       <div id="preview" class="w-2/5 flex flex-col justify-center items-center content-between">
         <label class="font-mono text-lg">预览结果：</label>
@@ -51,22 +62,23 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
-import { NUpload, NButton, NImage, useMessage, type UploadFileInfo } from 'naive-ui'
+import { NUpload, NButton, NImage, NDivider, useMessage, type UploadFileInfo } from 'naive-ui'
 
 const imageURLs = ref<[File | null, File | null]>([null, null])
 const newImageContent = ref<string | null>(null)
-const fileList = ref<UploadFileInfo[]>([])
+const fileListImg = ref<UploadFileInfo[]>([])
+const fileListGT = ref<UploadFileInfo[]>([])
 const loading = ref<boolean>(false)
 const message = useMessage()
 
-function handleUploadChange(options: {
+function handleUploadImgChange(options: {
   file: UploadFileInfo
   fileList: Array<UploadFileInfo>
   event?: Event
 }): void {
   console.log(options)
-  fileList.value = options.fileList
-  console.log(fileList.value)
+  fileListImg.value = options.fileList
+  console.log(fileListImg.value)
 }
 
 const handleFileListChange = () => {
@@ -77,7 +89,15 @@ const handleRemove = (data: { file: UploadFileInfo; fileList: UploadFileInfo[] }
   message.info('删除照片 ' + data.file.id)
 }
 
-const previewImageUrlRef1 = ref('')
+function handleUploadGTChange(options: {
+  file: UploadFileInfo
+  fileList: Array<UploadFileInfo>
+  event?: Event
+}): void {
+  console.log(options)
+  fileListGT.value = options.fileList
+  console.log(fileListGT.value)
+}
 
 async function confirmUpload() {
   loading.value = true
@@ -104,3 +124,22 @@ onMounted(() => {
   imageURLs.value = [null, null]
 })
 </script>
+
+<style scoped>
+.n-upload-file-list .n-upload-file.n-upload-file--image-card-type {
+  position: relative;
+  width: 416px;
+  height: 416px;
+  border: var(--n-item-border-image-card);
+  border-radius: var(--n-border-radius);
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition:
+    border-color 0.3s var(--n-bezier),
+    background-color 0.3s var(--n-bezier);
+  border-radius: var(--n-border-radius);
+  overflow: hidden;
+}
+</style>
